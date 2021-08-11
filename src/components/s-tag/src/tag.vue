ba0db2ef2c6f96dea69f560c52c345f1b703599d
pengtao <pengtao_it@163.com>
2021-08-11 16:01:27 +0800

    修复tag组件bug

--- src/components/s-tag/src/tag.vue
+++ src/components/s-tag/src/tag.vue
@@ -2,13 +2,17 @@
  * @Author: PT
  * @Date: 2021-08-03 17:05:24
  * @LastEditors: PT
- * @LastEditTime: 2021-08-03 17:08:48
+ * @LastEditTime: 2021-08-11 16:01:00
  * @Description: file content
 -->
 <script>
 export default {
   name: 'STag',
-  inject: [ 'elForm' ],
+  inject: {
+    elForm: {
+      default: null
+    }
+  },
   render () {
     let size = this.$attrs.size || (this.elForm || {}).size
     return (
